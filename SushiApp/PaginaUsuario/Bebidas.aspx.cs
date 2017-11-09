afa00076903ee7445a9da968a54d588b57505f7c
CategoriaWill <wi.rivera@alumnos.duoc.cl>
2017-11-09 03:57:04 -0300

    Modificado code-behind de bebidas. Se intentó crear un DataTable pero al correo se cae por una exception... Se debe revisar el código... TODO: Crear un GridView en ShopBasket.aspx para que reciba desde la variable Session la lista de compras, una vez sanjado el tema de pasarle la lista a la Session. Tal vez se deba intentar con otro tipo de lista y no un DataTable...

--- SushiApp/PaginaUsuario/Bebidas.aspx.cs
+++ SushiApp/PaginaUsuario/Bebidas.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using SushiApp.Models;
+using System.Data;
 
 namespace SushiApp.PaginaUsuario
 {
@@ -13,13 +14,57 @@ namespace SushiApp.PaginaUsuario
         wsProducto.ServiceProductoClient ProductoClient = new wsProducto.ServiceProductoClient();
         wsProducto.producto auxProducto = new wsProducto.producto();
 
-        int _productoId;
-        String _nombreProducto;
-        String _imagenProducto;
-        int _precioUnitario;
-        int _cantidad;
-        int _descuento;
-        int _precioTotal;
+        //int _productoId;
+        //String _nombreProducto;
+        //String _imagenProducto;
+        //int _precioUnitario;
+        //int _cantidad;
+        //int _descuento;
+        //int _precioTotal;
+
+        DataTable dtb;
+        
+
+        public void CargarDetalle()
+        {
+            dtb = new DataTable("Carrito");
+            dtb.Columns.Add("ID_PRODUCTO", System.Type.GetType("System.Int32"));
+            dtb.Columns.Add("NOMBRE_PRODUCTO", System.Type.GetType("System.String"));
+            dtb.Columns.Add("PRECIO_PRODUCTO", System.Type.GetType("System.Int32"));
+            dtb.Columns.Add("IMAGEN_PRODUCTO", System.Type.GetType("System.String"));
+            dtb.Columns.Add("SUBTOTAL", System.Type.GetType("System.Int32"));
+            dtb.Columns.Add("CANTIDAD_PRODUCTO", System.Type.GetType("System.Int32"));
+
+            Session["Pedido"] = dtb;
+        }
+
+        public void AgregarItem(int id, string nombre, int precio, string imagen)
+        {
+            int total;
+            int cantidad = 1;
+            total = precio * cantidad;
+            DataTable Carrito = (DataTable)Session["Pedido"];
+            //if ((DataTable)Session["Pedido"] == null)
+            //{
+            //    Carrito = new DataTable();
+            //}
+            //else
+            //{
+            //    Carrito = (DataTable)Session["Pedido"];
+            //}
+
+            
+            DataRow fila = Carrito.NewRow();
+            fila[0] = id;
+            fila[1] = imagen;
+            fila[2] = nombre;
+            fila[3] = precio;
+            fila[4] = (int)cantidad;
+            fila[5] = total;
+            Carrito.Rows.Add(fila);
+            Session["Pedido"] = Carrito;
+        }
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -67,23 +112,32 @@ namespace SushiApp.PaginaUsuario
             if (e.CommandName == "Agregar")
             {
 
-                string id = e.CommandArgument.ToString();
+                //String id = e.CommandArgument.ToString();
+                int _productoId;
+                String _nombreProducto = null, _imagenProducto = null;
+                int _precioUnitario = 0;
+                //int _cantidad = 1;
 
-                Response.Write(id);
+                //Response.Write(id);
 
                 _productoId = Convert.ToInt32(e.CommandArgument.ToString());
                 _nombreProducto = ((Label)e.Item.FindControl("lblNombreProducto")).Text;
                 _imagenProducto = ((Image)e.Item.FindControl("imgImagenProducto")).ImageUrl;
                 _precioUnitario = Convert.ToInt32(((Label)e.Item.FindControl("lblPrecioProducto")).Text);
-                _cantidad = 1;
+                
                 //_descuento = 
                 //_precioTotal;
 
-                Carro prodCarro = new Carro();
-                prodCarro.ProductoId = _productoId;
-                prodCarro.NombreProducto = _nombreProducto;
-                prodCarro.ImagenProducto = _imagenProducto;
-                prodCarro.PrecioUnitario = _precioUnitario;
+                AgregarItem(_productoId, _nombreProducto, _precioUnitario, _imagenProducto);
+                Session["prueba"] = "Sesión usuario prueba";
+
+                //Carro prodCarro = new Carro();
+                //prodCarro.ProductoId = _productoId;
+                //prodCarro.NombreProducto = _nombreProducto;
+                //prodCarro.ImagenProducto = _imagenProducto;
+                //prodCarro.PrecioUnitario = _precioUnitario;
+
+
 
 
 
